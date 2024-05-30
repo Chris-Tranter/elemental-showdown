90da95e88b48a5078e2c3490f1047c636d1c5d4e
Waris Gaspard <warisgaspard@gmail.com>
2024-05-30 04:06:15 +0000

    add end-game modal for anonymous game

--- scripts/anonymous-game.js
+++ scripts/anonymous-game.js
@@ -44,8 +44,11 @@ function playGame(playerChoice) {
     const finalResult = document.getElementById("finalResult");
     if (playerScore > computerScore) {
       finalResult.textContent = "You win the game!";
+      endOfGame('player')
     } else if (playerScore < computerScore) {
       finalResult.textContent = "Computer wins the game!";
+      endOfGame('computer')
+
     } else {
       finalResult.textContent = "It's a tie game!";
     }
@@ -67,30 +70,30 @@ function resetGame() {
   round = 1;
   updateScore();
   console.clear();
-  battlesWon = battlesWon + 1;
 }
 
-// Lose modal
+// End of Game-anonymous modal
 var modal = document.getElementById("end-game-modal");
 var playAgainBtn = document.getElementById("play-again");
 var closeModalBtn = document.getElementsByClassName("close")[0];
 var exitGameBtn = document.getElementById("admit-defeat");
-var endScore = document.getElementById("battles-won")
+var endResult = document.getElementById("end-result")
 var endMessage = document.getElementById("end-message")
 
-openModalBtn.onclick = function () {
+function endOfGame(winner) {
   modal.style.display = "block";
-  endScore.innerText = battlesWon;
-  if (battlesWon === 0) {
-    endMessage.innerText = `You won ${battlesWon} battles. Surely you can make it further than that? `
-  } else if (battlesWon === 1) {
-    endMessage.innerText = `You won ${battlesWon} battle! Congratulations you beat The Singularity once. Can you do it again?`
-  } else { 
-    endMessage.innerText = `You won ${battlesWon} battles! But can you win the war?`
+  if (winner === 'player') {
+    endResult.innerText = `You won the Elemental Showdown!`
+    endMessage.innerText = `You can now rest on your laurels or join the fight again!`
+    playAgainBtn.innerText = `Play Again!`
+    exitGameBtn.innerText = `Leave with Pride`
+  } else {
+    endResult.innerText = `You lost the Elemental Showdown...`
+    endMessage.innerText = `The Singularity may have won this battle but you could win the war...`
+    playAgainBtn.innerText = `Try Again!`
+    exitGameBtn.innerText = `Accept Defeat...`
   };
-  
-  
-};
+}
 
 closeModalBtn.onclick = function () {
   modal.style.display = "none";
